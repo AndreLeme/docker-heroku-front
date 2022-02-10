7dc2b497e7b385914db9c8c1351ce775754b95e6
Andre Leme <andresleme@gmail.com>
2022-02-10 17:55:06 -0300

    fixed api endpoint

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 import React, { useEffect, useState } from 'react';
 
-const API_ENDPOINT = 'http://localhost:3000/users';
+const API_ENDPOINT = 'https://heroku-backend-77.herokuapp.com/users';
 
 function App() {
   const [data, setData] = useState([]);
